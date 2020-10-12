5fbcd3ec50d29b0bdeda63a261482588e5d6feda
Adam <silvertitaniumx@gmail.com>
2020-10-12 12:57:34 -0700

    updating files

--- Mapping_Single_points/static/js/logic.js
+++ Mapping_Single_points/static/js/logic.js
@@ -11,9 +11,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
