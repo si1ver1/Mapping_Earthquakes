9aa0af70607688ff37aa064361e172a299f7dc62
Adam <silvertitaniumx@gmail.com>
2020-10-13 21:50:19 -0700

    adding step 4

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -28,19 +28,6 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/si1ver1/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
-// Create a style for the lines.
-/*
-let myStyle = {
-  fillColor: "#ffffa1",
-  fillOpacity: 0.25,
-  weight: 1,
-  color: 'blue'
-}
-*/
-
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
